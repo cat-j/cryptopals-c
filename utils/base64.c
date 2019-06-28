1ed6a9db2cc353d71f00ffd386d7c833bead1ae4
cat-j <catalinajuarros@gmail.com>
2019-06-28 14:24:16 -0300

    Write code for base64 encoding

---
+++ utils/base64.c
@@ -0,0 +1,47 @@
+#include "base64.h"
+
+#define SEXTETS_IN_QWORD 10
+#define QWORD_SIZE 64
+#define SEXTET_SIZE 6
+#define SEXTET_MASK 0x3F
+
+/*
+Convert ASCII string to base64.
+Designed with readability (rather than speed) in mind.
+*/
+char* hex_to_base64(char* ascii, uint64_t len) {
+    uint64_t k = 0;
+    uint64_t n_bits = len << 3;
+    uint64_t n_sextets = n_bits / 6;
+    uint64_t remaining_bits = n_bits % 6; // either 0, 2 or 4
+    char data, next_highest_bits, sextet;
+    
+    uint64_t encoded_len = n_sextets + (remaining_bits >> 1);
+    char* encoded = malloc((encoded_len + 1) * sizeof(char));
+    
+    for (uint64_t i = 0; i < len; ++i) {
+        data = ascii[i];
+
+        if (i%3 == 0) {
+            sextet = data >> 2; // higher 6 bits
+            next_highest_bits = (data & 0x3) << 4; // keep lower 2
+        } else if (i%3 == 1) {
+            sextet = (data >> 4) | next_highest_bits; // 2 lower prev ++ 4 higher
+            next_highest_bits = (data & 0xF) << 2; // keep lower 4
+        } else {
+            sextet = (data >> 6) | next_highest_bits; // 4 lower prev ++ 2 higher
+        }
+
+        encoded[k++] = char_table[sextet];
+        if (i%3 == 2) encoded[k++] = char_table[data & 0x3F]; // extra 6 bits to process
+    }
+
+    if (remaining_bits != 0) {
+        encoded[k++] = char_table[next_highest_bits];
+        encoded[k++] = '=';
+        if (remaining_bits == 4) encoded[k++] = '=';
+    }
+
+    encoded[k++] = NULL;
+    return encoded;
+}
