306be7301473c6265b3998924724a0d5a282e063
Cat <catalinajuarros@gmail.com>
2019-06-28 21:29:41 -0300

    Solve challenge 1

--- src/challenge1/main.c
+++ src/challenge1/main.c
@@ -1,12 +1,13 @@
 #include "../utils/base64.h"
 #include "../utils/fileutils.h"
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char filename[8] = "data.txt";
+    char filename[8] = "byteData";
     int64_t bytes_read;
     char* data = read_file(filename, &bytes_read);
-    printf("%s\n", data);
-    // printf("%s\n", encoded);
+    char* encoded = hex_to_base64(data, strlen(data));
+    printf("%s\n", encoded);
     return 0;
 }
