0e061f879b064a1ef803ed7002c7585c7d8b9f4c
Cat <catalinajuarros@gmail.com>
2019-06-28 21:29:26 -0300

    Convert hex representation to bytes

---
+++ scripts/writebytes.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser
+
+def getArgs():
+    parser = ArgumentParser()
+    parser.add_argument("src", help="Source file: name of file containing ASCII representation of hex string")
+    parser.add_argument("dst", help="Destination file: name of file to write the results to")
+    args = parser.parse_args()
+    return(args.src, args.dst)
+
+# Generate raw byte data for challenges
+def main():
+    (srcName, dstName) = getArgs()
+    dst = open(dstName, 'wb')
+    with open(srcName, 'r+') as src:
+        hexString = src.read()
+        dst.write(bytearray.fromhex(hexString))
+    dst.close()
+    return
+
+main()
