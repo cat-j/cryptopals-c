9a18daee2a114657636a769649ac7a59a1709fa5
Cat <catalinajuarros@gmail.com>
2019-07-01 08:41:21 -0300

    Replace magic constant with define

--- src/challenge4/main.c
+++ src/challenge4/main.c
@@ -2,17 +2,21 @@
 #include "../utils/encryption.h"
 #include <stdint.h>
 
+#define N_BEST_PLAINTEXTS 10
+
 int main() {
     char filename[] = "byteData";
     int64_t bytes_per_line, lines_read;
-    double scores[10];
+    double scores[N_BEST_PLAINTEXTS];
     char** ciphertexts = hex_decode_file_lines(filename, &bytes_per_line, &lines_read);
-    char** keys = malloc(10 * sizeof(char));
-    for (int i = 0; i < 10; ++i) {
+    
+    char** keys = malloc(N_BEST_PLAINTEXTS * sizeof(char));
+    for (int i = 0; i < N_BEST_PLAINTEXTS; ++i) {
         keys[i] = malloc(1);
     }
+
     char** best_plaintexts = get_n_best_plaintexts(ciphertexts, bytes_per_line, keys,
-        lines_read, 10, scores);
+        lines_read, N_BEST_PLAINTEXTS, scores);
     
     for (int i = 0; i < lines_read; ++i) {
         printf("%s\n", ciphertexts[i]);
