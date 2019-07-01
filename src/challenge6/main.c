5e8ee69e846c7d037fbf18642d812646aa070f26
cat-j <catalinajuarros@gmail.com>
2019-07-01 13:16:55 -0300

    Start challenge 6

---
+++ src/challenge6/main.c
@@ -0,0 +1,18 @@
+#include "../utils/hamming.h"
+#include "../utils/base64.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[] = "this is a test";
+    char str2[] = "wokka wokka!!!";
+    uint64_t distance = hamming_distance(str1, str2, strlen(str1));
+    printf("%ld\n", distance);
+
+    char encoded[] = "TWFu";
+    char* decoded = base64_decode(encoded, strlen(encoded));
+    printf("%s\n", decoded);
+
+    return 0;
+}
